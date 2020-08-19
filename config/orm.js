9b934cc9905fd4f7d25f0fcf4fbbb514e0fe4c51
KekeliVa <kekeli.afantchao@yahoo.com>
2020-08-19 18:31:57 -0400

    added my first method to the orm file

--- config/orm.js
+++ config/orm.js
@@ -1,27 +1,37 @@
-/**@todo
- * method to add to departments table
- */
+const connection = require("connection.js");
 
- /**@todo
- * method to add to employee table
- */
-
- /**@todo
- * method to add to roles table
- */
-
- /**@todo
- * method to view departments table
- */
-
- /**@todo
- * method to view employees table
- */
-
- /**@todo
- * method to view roles table
- */
-
- /**@todo
- * method to add to update employee roles 
- */
+const orm = {
+  /**@todo
+   * method to add to departments table
+   */
+  departmentInsert: function (departmentName) {
+    let queryString = "INSERT INTO department (name) VALUES (??)";
+    connection.query(queryString, [departmentName], (err, result) => {
+      if (err) throw (err);
+      console.log(result);
+    } )
+  },
+   /**@todo
+   * method to add to employee table
+   */
+  
+   /**@todo
+   * method to add to roles table
+   */
+  
+   /**@todo
+   * method to view departments table
+   */
+  
+   /**@todo
+   * method to view employees table
+   */
+  
+   /**@todo
+   * method to view roles table
+   */
+  
+   /**@todo
+   * method to add to update employee roles 
+   */
+}
